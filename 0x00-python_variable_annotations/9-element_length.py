e5cb750e12e90ff36c39f72118d8fe76c6163f59
ElvisMw <elvismwanthi@gmail.com>
2024-05-02 17:08:45 +0300

    pep 8 fix- long line

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,9 +3,19 @@
 9-element_length module
 Contains a type-annotated function element_length
 """
-from typing import Iterable, Sequence, Tuple
+from typing import Iterable, Sequence, Tuple, List
 
 
-def element_length(lst: Iterable[Sequence]) -> list[Tuple[Sequence, int]]:
-    """Annotates the function element_length"""
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Returns a list of tuples containing each element of the
+    input sequence `lst` and its length.
+
+    Parameters:
+        lst (Iterable[Sequence]): An iterable containing sequences.
+
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples where each
+        tuple contains a sequence from `lst` and its length.
+    """
     return [(i, len(i)) for i in lst]

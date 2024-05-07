25ae0c446b40223f73ccd5a2743a437b575a1538
ElvisMw <elvismwanthi@gmail.com>
2024-05-07 19:16:51 +0300

    4 -task

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
+"""
+The task_wait_n function creates a list of asyncio Tasks, each of which runs
+the task_wait_random function with the given max_delay argument.
+
+The function then waits for all of the tasks to complete using asyncio.gather
+and returns the sorted list of wait times.
+"""
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
 
 
-async def task_wait_random(max_delay: int) -> float:
-    """
-    Asynchronous coroutine that generates a random delay
-    between 0 and max_delay.
-    """
-    return await wait_random(max_delay)
+task_wait_random = __import__('3-tasks').task_wait_random
+
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Executes multiple coroutines at the same time with async
+    Creates a list of asyncio Tasks, waits for all of them to complete, and
+    returns the sorted list of wait times.
+
+    Arguments:
+        n: The number of tasks to create.
+        max_delay: The maximum delay to pass to each task.
+
+    Returns:
+        The sorted list of wait times.
     """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+
+    wait_tasks = [task_wait_random(max_delay) for _ in range(n)]
+    wait_times = await asyncio.gather(*wait_tasks)
+    return sorted(wait_times)

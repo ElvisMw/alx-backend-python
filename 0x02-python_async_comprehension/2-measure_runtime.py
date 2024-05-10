95ef5ddb8713afffa7e07e7317e047a396e5738e
ElvisMw <elvismwanthi@gmail.com>
2024-05-10 21:08:43 +0300

    error fix - run in running 2-measure.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 """
-2-measure_runtime
+Module that measures the runtime of four calls to async_comprehension
 """
-
 import asyncio
 import time
 from importlib import import_module as using
 
 
+"""Async function that collects 10 random numbers"""
 async_comprehension = using('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """
-    measure_runtime
+    Measures the total runtime of four calls to
+    async_comprehension
     """
+
     start_time = time.time()
 
+    """ Executes 4 calls to async_comprehension concurrently
+    gather() returns a list of futures"""
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
+
+    """ Subtract start time from current time to get total runtime"""
     return time.time() - start_time

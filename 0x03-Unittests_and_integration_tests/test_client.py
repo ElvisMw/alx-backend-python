3a0fd7a6b93a38808aaf604ab0a0c64ac2b14779
ElvisMw <elvismwanthi@gmail.com>
2024-05-27 23:14:31 +0300

    task 9

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,58 +1,31 @@
 #!/usr/bin/env python3
-
-"""
-Integration tests for GithubOrgClient class.
-"""
-
 import unittest
 from unittest.mock import patch
 from parameterized import parameterized_class
-
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 from client import GithubOrgClient
 
 
 @parameterized_class(
-    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
-    [
+    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"), [
         (org_payload, repos_payload, expected_repos, apache2_repos),
     ]
 )
 class TestIntegrationGithubOrgClient(unittest.TestCase):
-    """
-    Test case for GithubOrgClient class.
-    """
-
     @classmethod
     def setUpClass(cls):
-        """
-        Set up the test environment.
-        """
         cls.get_patcher = patch('client.requests.get')
-
         cls.mock_get = cls.get_patcher.start()
 
     @classmethod
     def tearDownClass(cls):
-        """
-        Tear down the test environment.
-        """
         cls.get_patcher.stop()
 
     def setUp(self):
-        """
-        Reset the mock object before each test.
-        """
         self.mock_get.reset_mock()
 
     def test_public_repos(self):
-        """
-        Test the public_repos property.
-        """
-        # Patch the return value of the requests.get method
-        self.mock_get.return_value.json.side_effect = [
-            self.org_payload, self.repos_payload
-        ]
+        self.mock_get.side_effect = [self.org_payload, self.repos_payload]
 
         client = GithubOrgClient("test-org")
         result = client.public_repos
@@ -60,13 +33,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, self.expected_repos)
 
     def test_public_repos_with_license(self):
-        """
-        Test the public_repos property with Apache 2.0 license.
-        """
-        # Patch the return value of the requests.get method
-        self.mock_get.return_value.json.side_effect = [
-            self.org_payload, self.apache2_repos
-        ]
+        self.mock_get.side_effect = [self.org_payload, self.apache2_repos]
 
         client = GithubOrgClient("test-org")
         result = client.public_repos

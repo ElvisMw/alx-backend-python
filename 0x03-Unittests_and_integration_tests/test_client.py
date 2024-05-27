fcdbb2e23d7a2c943cb258f95dd890cb3f52dfd4
ElvisMw <elvismwanthi@gmail.com>
2024-05-27 23:02:53 +0300

    task 7

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
 import unittest
 from unittest.mock import patch
+from parameterized import parameterized
 from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    @patch('client.GithubOrgClient.get_json')
-    @patch('client.GithubOrgClient._public_repos_url',
-           new_callable=PropertyMock)
-    def test_public_repos(self, mock_public_repos_url, mock_get_json):
-        mock_public_repos_url.return_value =
-        "https://api.github.com/orgs/test-org/repos"
-        mock_get_json.return_value = [{"name": "repo1"}, {"name": "repo2"}]
-
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
         client = GithubOrgClient("test-org")
-        result = client.public_repos
-
-        mock_get_json.assert_called_once_with
-        ("https://api.github.com/orgs/test-org/repos")
-        self.assertEqual(result, ["repo1", "repo2"])
+        result = client.has_license(repo, license_key)
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':

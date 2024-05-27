e105638d659d75e3e10ee5fc4eee61ab1d508f63
ElvisMw <elvismwanthi@gmail.com>
2024-05-27 22:44:09 +0300

    task 3- test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python3
 import unittest
-from unittest.mock import patch, Mock
-from parameterized import parameterized
-from utils import get_json
+from unittest.mock import patch
+from utils import memoize
 
 
-class TestGetJson(unittest.TestCase):
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
-    ])
-    @patch('utils.requests.get')
-    def test_get_json(self, test_url, test_payload, mock_get):
-        # Set up the mock to return the test payload
-        mock_get.return_value = Mock(status_code=200)
-        mock_get.return_value.json.return_value = test_payload
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
 
-        # Call the function being tested
-        result = get_json(test_url)
+            @memoize
+            def a_property(self):
+                return self.a_method()
 
-        # Check that the mock was called correctly
-        mock_get.assert_called_once_with(test_url)
-        # Check that the function returned the expected result
-        self.assertEqual(result, test_payload)
+        with patch.object(TestClass, 'a_method', return_value=42)\
+                as mock_method:
+            instance = TestClass()
+            self.assertEqual(instance.a_property, 42)
+            self.assertEqual(instance.a_property, 42)
+            mock_method.assert_called_once()
 
 
 if __name__ == '__main__':
